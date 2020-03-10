d9d6410e7cd5d3c67dc7e30ff0efbe9f2df24be2
Theo Nga <tdbvume@outlook.com>
2020-03-10 23:19:44 +0800

    server_gitignore

--- domains/settings.py
+++ domains/settings.py
@@ -101,14 +101,14 @@ WSGI_APPLICATION = 'domains.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -119,7 +119,6 @@ DATABASES = {
         'PORT': 5432
     }
 }
-'''
 
 
 # Password validation
